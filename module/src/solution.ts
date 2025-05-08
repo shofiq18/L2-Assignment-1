fc692519e67a0e60f4976f4a5fdf749e94d48a21
shofiq18 <shafiqul.islam25021998@gmail.com>
2025-05-08 19:33:38 +0600

    added solution 2

--- module/src/solution.ts
+++ module/src/solution.ts
@@ -17,3 +17,31 @@ const result3 = formatString("Hello", false);
 
 
 
+const books = [
+    {title: "Book A", rating: 4.5}, 
+    {title: "Book B", rating: 3.2}, 
+    {title: "Book C", rating: 5.0}, 
+    {title: "Book D", rating: 4.2}, 
+    {title: "Book E", rating: 3.5}, 
+
+]
+
+
+
+function filterByRating(items: {title: string, rating: number}[]) : {title: string, rating: number} [] {
+    return items.filter(item=> item.rating >= 4);
+}
+
+const result4 = filterByRating(books);
+
+
+
+
+
+
+
+
+
+
+
+
