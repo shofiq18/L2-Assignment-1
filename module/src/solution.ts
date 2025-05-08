1f615dd98c5462c76d0e776b7944dd067d5ed1d6
shofiq18 <shafiqul.islam25021998@gmail.com>
2025-05-08 23:17:27 +0600

    added readme file

--- module/src/solution.ts
+++ module/src/solution.ts
@@ -130,14 +130,14 @@ enum Day {
 
 
 
-  
+
 
 
   async function squareAsync(n: number): Promise<number> {
         return new Promise ((resolve, reject) => {
             if (n < 0) {
                 reject(new Error("Negative number not allowed"));
-            }
+            }  
             setTimeout(() => {
                 resolve(n * n);
             }, 1000);
