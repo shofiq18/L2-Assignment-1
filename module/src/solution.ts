02c4239f68e84828827fc080ca8a88b58825b38e
shofiq18 <shafiqul.islam25021998@gmail.com>
2025-05-08 16:29:46 +0600

    added soluton 1

--- module/src/solution.ts
+++ module/src/solution.ts
@@ -1,1 +1,19 @@
 
+
+
+function formatString(input: string, toUpper?: boolean): string {
+ if(toUpper === false) {
+    return input.toLowerCase();
+ }else{
+    return input.toUpperCase();
+ }
+ 
+}
+const result1 = formatString("Hello");
+const result2 = formatString("Hello", true);
+const result3 = formatString("Hello", false);
+
+
+
+
+
